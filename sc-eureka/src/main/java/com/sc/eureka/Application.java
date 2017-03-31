ab359ed3ab9e58f15a53d74bc38c03e8ffed7c51
zhubin <125777171@qq.com>
2017-03-31 15:23:55 +0800

    move package

---
+++ sc-eureka/src/main/java/com/sc/eureka/Application.java
@@ -0,0 +1,9 @@
+package com.sc.eureka;
+
+public class Application {
+	
+	public static void main(String[] args){
+		
+	}
+
+}
--- sc-eureka/src/main/java/com/sc/learning/helloworld/Application.java
+++
@@ -1,9 +0,0 @@
-package com.sc.learning.helloworld;
-
-public class Application {
-	
-	public static void main(String[] args){
-		
-	}
-
-}
